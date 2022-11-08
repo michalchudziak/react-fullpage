7e4e054f461f494cd0460581e699ca6db637cb05
Michał Chudziak <michal.chudziak@gmail.com>
2022-11-08 18:50:45 +0100

    remove fp id

--- components/selectors.js
+++ components/selectors.js
@@ -1,4 +1,4 @@
-export const MAIN_SELECTOR = 'fullpage';
-export const ID_SELECTOR = `#${MAIN_SELECTOR}`;
+export const MAIN_SELECTOR = 'test';
+export const ID_SELECTOR = `#fullpage`;
 export const DEFAULT_SECTION = '.section';
 export const DEFAULT_SLIDE = '.SLIDE';
